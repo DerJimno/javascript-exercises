05ecb55f3aebdc2a4c1be64f9d9198184b76fec4
jimno <98581961+DerJimno@users.noreply.github.com>
2025-03-18 12:27:14 +0100

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+  if (!Number.isInteger(numA) || !Number.isInteger(numB)) return "ERROR";
+  if (numA < 0 || numB < 0) return "ERROR";
+  if (numA > numB) [numB, numA] = [numA, numB];
 
+  let sum = 0;
+  for (let i = numA; i < numB + 1; i++) {
+    sum += i 
+  };
+  return sum
 };
-
 // Do not edit below this line
 module.exports = sumAll;
